2d9a8941dc3435f402d726af0b4a50112e016bb6
JSterling8 <u1276062@unimail.hud.ac.uk>
2014-02-18 14:15:09 +0000

    Test push.

---
+++ src/OOSD_Assignment.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : OOSD_Assignment.cpp
+// Author      : Jonathan Sterling
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "I'm C++ and I break a lot." << endl; // prints I'm C++ and I break a lot.
+	return 0;
+}
